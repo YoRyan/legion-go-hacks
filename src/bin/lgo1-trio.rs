8613ecf34787129d23d8616edb203b74c81269f4
Ryan Young <ryan@youngryan.com>
2026-02-24 05:57:02 +0000

    feat: switch tablet mode off when a keyboard is docked
    Rationale: There isn't a reliable way to suppress the on-screen keyboard as there isn't a public API exposed. So, just tell Gnome we are no longer a tablet. (We will still need an extension to fix the screen rotation.)

--- src/bin/lgo1-trio.rs
+++ src/bin/lgo1-trio.rs
@@ -1,5 +1,6 @@
-use std::collections::{HashSet, VecDeque};
-use std::sync::{Arc, Mutex, mpsc};
+use std::collections::HashSet;
+use std::sync::atomic::{AtomicU32, Ordering};
+use std::sync::{Arc, mpsc};
 use std::thread;
 use std::time::Duration;
 
@@ -8,11 +9,8 @@ use dbus_crossroads::Crossroads;
 use evdev::{AttributeSet, BusType, EventType, InputEvent, KeyCode, SwitchCode};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-/// A changed_properties array suitable for a PropertiesChanged signal.
-/// See https://github.com/diwic/dbus-rs/blob/master/dbus/examples/argument_guide.md.
-type ChangedProps<'a> = Vec<(&'a str, dbus_arg::Variant<Box<dyn dbus_arg::RefArg>>)>;
-type ChangedPropsQueue<'a> = VecDeque<ChangedProps<'a>>;
 
+#[derive(Clone, Copy, Debug, PartialEq)]
 enum KeyboardStatus {
     /// The keyboard case is connected.
     CaseExternal = 0x2,
@@ -21,29 +19,46 @@ enum KeyboardStatus {
     /// No external keyboard is connected.
     None = 0x0,
 }
+impl KeyboardStatus {
+    fn from_u32(value: u32) -> KeyboardStatus {
+        match value {
+            0x2 => KeyboardStatus::CaseExternal,
+            0x1 => KeyboardStatus::AnyExternal,
+            0x0 | _ => KeyboardStatus::None,
+        }
+    }
 
-struct DBusObject {
-    keyboard_status: u32,
+    fn is_tablet_mode(&self) -> bool {
+        *self == KeyboardStatus::None
+    }
 }
 
 const DBUS_OBJECT_PATH: &str = "/com/youngryan/LGo1Trio";
 const DBUS_INTERFACE: &str = "com.youngryan.LGo1Trio";
+const FORWARD_KEYS: [KeyCode; 2] = [KeyCode::KEY_VOLUMEDOWN, KeyCode::KEY_VOLUMEUP];
 
 fn main() {
-    spawn_loop("run_virtual_device", run_virtual_device);
+    let atomic_status = Arc::new(AtomicU32::new(0x0));
+    let atomic_status2 = atomic_status.clone();
+
+    // (We pass references and Arc clones make the functions callable multiple
+    // times.)
+
+    let (virtual_s, virtual_r) = mpsc::channel::<InputEvent>();
+    let virtual_s2 = virtual_s.clone();
+    spawn_loop("read_suppressed_keyboard", move || {
+        read_suppressed_keyboard(&virtual_s)
+    });
+    spawn_loop("run_virtual_device", move || run_virtual_device(&virtual_r));
 
     let (udev_s, udev_r) = mpsc::sync_channel::<()>(0);
-    // A reference or Arc is necessary to make the function callable multiple times.
-    spawn_loop("read_udev_events", move || read_udev_events(&udev_s));
-
-    let dbus_cr = Arc::new(Mutex::new(make_dbus_crossroads()));
-    let dbus_cr2 = dbus_cr.clone();
-    let cpq = Arc::new(Mutex::new(VecDeque::<ChangedProps>::new()));
-    let cpq2 = cpq.clone();
-    spawn_loop("read_keyboard_status", move || {
-        read_keyboard_status(dbus_cr.clone(), &udev_r, cpq.clone())
+    spawn_loop("read_udev_add_remove", move || {
+        read_udev_add_remove(&udev_s)
     });
-    let _ = spawn_loop("run_dbus", move || run_dbus(dbus_cr2.clone(), cpq2.clone())).join();
+    spawn_loop("read_keyboard_status", move || {
+        read_keyboard_status(&udev_r, &virtual_s2, atomic_status.clone())
+    });
+    let _ = spawn_loop("run_dbus", move || run_dbus(atomic_status2.clone())).join();
 
     unreachable!();
 }
@@ -65,8 +80,7 @@ where
     })
 }
 
-fn run_virtual_device() -> Result<()> {
-    const FORWARD_KEYS: [KeyCode; 2] = [KeyCode::KEY_VOLUMEDOWN, KeyCode::KEY_VOLUMEUP];
+fn read_suppressed_keyboard(consumer: &mpsc::Sender<InputEvent>) -> Result<()> {
     let forward_codes: HashSet<u16> = FORWARD_KEYS.iter().map(|k| k.0).collect();
 
     let mut internal_keyboard = evdev::enumerate()
@@ -77,6 +91,17 @@ fn run_virtual_device() -> Result<()> {
         })
         .ok_or("could not find internal keyboard")?;
 
+    loop {
+        for event in internal_keyboard.fetch_events()? {
+            let code = event.code();
+            if forward_codes.contains(&code) {
+                consumer.send(InputEvent::new(EventType::KEY.0, code, event.value()))?;
+            }
+        }
+    }
+}
+
+fn run_virtual_device(event_stream: &mpsc::Receiver<InputEvent>) -> Result<()> {
     let keys = AttributeSet::<KeyCode>::from_iter(FORWARD_KEYS.iter());
     let switches = AttributeSet::<SwitchCode>::from_iter([SwitchCode::SW_TABLET_MODE]);
     let mut device = evdev::uinput::VirtualDevice::builder()?
@@ -91,35 +116,12 @@ fn run_virtual_device() -> Result<()> {
     )])?;
 
     loop {
-        for event in internal_keyboard.fetch_events()? {
-            let code = event.code();
-            if forward_codes.contains(&code) {
-                device.emit(&[InputEvent::new(EventType::KEY.0, code, event.value())])?;
-            }
-        }
+        let event = event_stream.recv()?;
+        device.emit(&[event])?;
     }
 }
 
-fn make_dbus_crossroads() -> Crossroads {
-    let mut cr = Crossroads::new();
-    let iface_token = cr.register(
-        DBUS_INTERFACE,
-        |b: &mut dbus_crossroads::IfaceBuilder<DBusObject>| {
-            b.property("KeyboardStatus")
-                .get(|_, obj| Ok(obj.keyboard_status));
-        },
-    );
-    cr.insert(
-        DBUS_OBJECT_PATH,
-        &[iface_token],
-        DBusObject {
-            keyboard_status: KeyboardStatus::None as u32,
-        },
-    );
-    cr
-}
-
-fn read_udev_events(notify: &mpsc::SyncSender<()>) -> Result<()> {
+fn read_udev_add_remove(consumer: &mpsc::SyncSender<()>) -> Result<()> {
     use std::os::unix::io::AsRawFd;
 
     let socket = udev::MonitorBuilder::new()?
@@ -153,7 +155,7 @@ fn read_udev_events(notify: &mpsc::SyncSender<()>) -> Result<()> {
         };
         match event.event_type() {
             udev::EventType::Add | udev::EventType::Remove => {
-                let _ = notify.try_send(());
+                let _ = consumer.try_send(());
             }
             _ => {}
         }
@@ -161,41 +163,32 @@ fn read_udev_events(notify: &mpsc::SyncSender<()>) -> Result<()> {
 }
 
 fn read_keyboard_status(
-    cr: Arc<Mutex<Crossroads>>,
-    wait_for: &mpsc::Receiver<()>,
-    signal: Arc<Mutex<ChangedPropsQueue>>,
+    udev_add_remove: &mpsc::Receiver<()>,
+    virtual_consumer: &mpsc::Sender<InputEvent>,
+    atomic_status: Arc<AtomicU32>,
 ) -> Result<()> {
     loop {
+        let status = keyboard_status();
+        virtual_consumer.send(InputEvent::new(
+            EventType::SWITCH.0,
+            SwitchCode::SW_TABLET_MODE.0,
+            status.is_tablet_mode() as i32,
+        ))?;
+        atomic_status.store(status as u32, Ordering::Relaxed);
+
         // Wait for an update, but also force a recheck every now and then.
-        match wait_for.recv_timeout(Duration::from_secs(120)) {
+        match udev_add_remove.recv_timeout(Duration::from_secs(120)) {
             Err(mpsc::RecvTimeoutError::Timeout) => {}
             _ => {
                 // Wait for all events to come in, and then impose a short delay. This
                 // accounts for the time the kernel needs to add and remove devices.
                 loop {
-                    match wait_for.recv_timeout(Duration::from_millis(1000)) {
+                    match udev_add_remove.recv_timeout(Duration::from_millis(1000)) {
                         Err(mpsc::RecvTimeoutError::Timeout) => break,
                         _ => continue,
                     }
                 }
             }
-        }
-
-        let mut changes: ChangedProps = Vec::new();
-        let status = keyboard_status() as u32;
-        {
-            let mut cr_lock = cr.lock().unwrap();
-            let obj: &mut DBusObject = cr_lock.data_mut(&DBUS_OBJECT_PATH.into()).unwrap();
-
-            if obj.keyboard_status != status {
-                changes.push(("KeyboardStatus", dbus_arg::Variant(Box::new(status))));
-                obj.keyboard_status = status;
-            }
-        }
-
-        if changes.len() > 0 {
-            let mut signal_lock = signal.lock().unwrap();
-            signal_lock.push_back(changes);
         }
     }
 }
@@ -229,39 +222,70 @@ fn keyboard_status() -> KeyboardStatus {
     KeyboardStatus::None
 }
 
-fn run_dbus(cr: Arc<Mutex<Crossroads>>, to_send: Arc<Mutex<ChangedPropsQueue>>) -> Result<()> {
+fn run_dbus(atomic_status: Arc<AtomicU32>) -> Result<()> {
     use dbus::channel::{MatchingReceiver, Sender};
 
+    let mut cr = make_crossroads(atomic_status.clone());
     let conn = dbus::blocking::LocalConnection::new_system()?;
     conn.request_name("com.youngryan.LGo1Trio", false, true, false)?;
-
     conn.start_receive(
         dbus::message::MatchRule::new_method_call(),
         Box::new(move |msg, conn| {
-            let mut cr_lock = cr.lock().unwrap();
-            let _ = cr_lock.handle_message(msg, conn);
+            cr.handle_message(msg, conn).unwrap();
             true
         }),
     );
+
+    let mut last_status: Option<KeyboardStatus> = Option::None;
     loop {
         conn.process(Duration::from_millis(100))?;
-        {
-            let mut to_send_lock = to_send.lock().unwrap();
-            for changed_props in to_send_lock.drain(..) {
-                conn.send(
-                    dbus::Message::signal(
-                        &DBUS_OBJECT_PATH.into(),
-                        &"org.freedesktop.DBus.Properties".into(),
-                        &"PropertiesChanged".into(),
-                    )
-                    .append3(
-                        DBUS_INTERFACE,
-                        changed_props,
-                        dbus_arg::Array::new(std::iter::empty::<&str>()),
-                    ),
+
+        let status = KeyboardStatus::from_u32(atomic_status.load(Ordering::Relaxed));
+        if last_status.is_none_or(|s| s != status) {
+            let mut changed_props = dbus_arg::PropMap::new();
+            changed_props.insert(
+                "KeyboardStatus".to_owned(),
+                dbus_arg::Variant(Box::new(status as u32)),
+            );
+            changed_props.insert(
+                "TabletMode".to_owned(),
+                dbus_arg::Variant(Box::new(status.is_tablet_mode())),
+            );
+            conn.send(
+                dbus::Message::signal(
+                    &DBUS_OBJECT_PATH.into(),
+                    &"org.freedesktop.DBus.Properties".into(),
+                    &"PropertiesChanged".into(),
                 )
-                .map_err(|_| "failed to send properties changed message")?;
-            }
+                .append3(
+                    DBUS_INTERFACE,
+                    changed_props,
+                    dbus_arg::Array::new(std::iter::empty::<&str>()),
+                ),
+            )
+            .map_err(|_| "failed to send properties changed message")?;
+
+            last_status = Option::Some(status);
         }
     }
+}
+
+fn make_crossroads(atomic_status: Arc<AtomicU32>) -> Crossroads {
+    let mut cr = Crossroads::new();
+    let iface_token = cr.register(
+        DBUS_INTERFACE,
+        |b: &mut dbus_crossroads::IfaceBuilder<Arc<AtomicU32>>| {
+            b.property("KeyboardStatus").get(|_, obj| {
+                let status_value = obj.load(Ordering::Relaxed);
+                Ok(status_value)
+            });
+            b.property("TabletMode").get(|_, obj| {
+                let status_value = obj.load(Ordering::Relaxed);
+                let tablet_mode = KeyboardStatus::from_u32(status_value).is_tablet_mode();
+                Ok(tablet_mode)
+            });
+        },
+    );
+    cr.insert(DBUS_OBJECT_PATH, &[iface_token], atomic_status);
+    cr
 }
